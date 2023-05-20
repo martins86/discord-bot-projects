534a9f4910ca26e7e22d8fc6f4d44a9bf284e92b
Thiago Martins de Freitas <martins86@gmail.com>
2023-05-20 15:17:29 -0300

    feat: add config dotenv

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js')
+
+// dotenv configuration
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { TOKEN } = process.env
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(TOKEN)
