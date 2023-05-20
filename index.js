de60f3a0fbe76a54fcafb6c933c32490ed73fb27
Thiago Martins de Freitas <martins86@gmail.com>
2023-05-20 17:20:23 -0300

    feat: add interactions with chat

--- index.js
+++ index.js
@@ -12,20 +12,24 @@ client.commands = new Collection();
 // import commands
 const fs = require("node:fs");
 const path = require("node:path");
+const foldersPath = path.join(__dirname, "commands");
+const commandFolders = fs.readdirSync(foldersPath);
 
-const commandsPath = path.join(__dirname, "commands");
-const commandFiles = fs
-  .readdirSync(commandsPath)
-  .filter((file) => file.endsWith(".js"));
-
-for (const file of commandFiles) {
-  const command = require(path.join(commandsPath, file));
-  if ("data" in command && "execute" in command) {
-    client.commands.set(command.data.name, command);
-  } else {
-    console.log(
-      `O Comando ${command.data.name} não possue "data" ou "execute".`,
-    );
+for (const folder of commandFolders) {
+  const commandsPath = path.join(foldersPath, folder);
+  const commandFiles = fs
+    .readdirSync(commandsPath)
+    .filter((file) => file.endsWith(".js"));
+  for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ("data" in command && "execute" in command) {
+      client.commands.set(command.data.name, command);
+    } else {
+      console.log(
+        `O Comando ${command.data.name} não possue "data" ou "execute".`,
+      );
+    }
   }
 }
 
@@ -34,3 +38,29 @@ client.once(Events.ClientReady, (c) => {
 });
 
 client.login(TOKEN);
+
+// on interactions with the bot
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) {
+    console.error(`Comando ${interaction.commandName} não foi encontrado!.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+
+    const message = `Ops! Erro ao executar o comando ${interaction.commandName}`;
+
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({ content: message, ephemeral: true });
+    } else {
+      await interaction.reply({ content: message, ephemeral: true });
+    }
+  }
+});
